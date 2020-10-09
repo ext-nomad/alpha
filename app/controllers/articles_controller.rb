c47196679b558b76ab2b70f25fa654e0182f4324
ext-nomad <i.dwanderer@gmail.com>
2020-10-09 18:38:55 +0300

    add/ association user/articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    @article.user = user.first
     if @article.save
       flash[:notice] = 'Article successfully created.'
       redirect_to articles_path
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user # , class_name: "user", foreign_key: "user_id"
   validates :title, presence: true, length: { minimum: 6, maximum: 100 }
   validates :description, presence: true, length: { minimum: 10, maximum: 100 }
 end
