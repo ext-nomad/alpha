caf525fffe4fc09c08a0405a4658caebd44254de
ext-nomad <i.dwanderer@gmail.com>
2020-10-11 17:53:12 +0300

    add/ session helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.username.capitalize} welcome to the Alpha Blog!"
       redirect_to articles_path
     else
