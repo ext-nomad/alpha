38f8d43c4a9e5a1d5e23a23a17aebcc5fcdebe67
ext-nomad <i.dwanderer@gmail.com>
2020-10-11 17:37:26 +0300

    add/ session cotroller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'about', to: 'home#about'
   get 'signup', to: 'users#new'
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users, except: %i[new]
   resources :articles
 end
