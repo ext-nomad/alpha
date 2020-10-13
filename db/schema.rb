bdc29ec3c50f985fc92ba59777aca7fda86592f4
ext-nomad <i.dwanderer@gmail.com>
2020-10-13 16:13:59 +0300

    add/ articles-caegories many_to_many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_094414) do
+ActiveRecord::Schema.define(version: 2020_10_13_124745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
