88d82d487b66cc89da83ca0c6c4dcda9363a60df
ext-nomad <i.dwanderer@gmail.com>
2020-10-13 14:17:38 +0300

    add/ integration tests for categories_create_process

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,29 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.all
+  end
 
-  def show; end
+  def show
+    @category = Category.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = 'Category was successfully created'
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
