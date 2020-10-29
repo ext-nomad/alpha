11a029b0cd1a2a908fe8f0b93d350d4b3fb09dc8
ext-nomad <i.dwanderer@gmail.com>
2020-10-29 16:53:05 +0300

    add/ font-awesome via webpack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,11 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
+require("stylesheets/custom.scss");
 
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap";
-require("stylesheets/custom.scss");
+import "@fortawesome/fontawesome-free/css/all";
 
 $(document).on("turbolinks:load", function () {
   window.addEventListener("scroll", function () {
