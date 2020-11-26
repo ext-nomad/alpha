7d3e11f5ad641582206bac194dedbe07b38d9462
ext-nomad <i.dwanderer@gmail.com>
2020-11-26 21:10:42 +0300

    fix/ webpack propper scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
-require("stylesheets/custom.scss");
+require("stylesheets/application.scss");
 
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap";
